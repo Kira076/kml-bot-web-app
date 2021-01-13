374914490c4335e1b8be7c9416ac0bac9b672c4c
kira076 <j.morrisoncleary@gmail.com>
2021-01-14 03:04:47 +1100

    Updated placeholding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Placeholder />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Placeholder from "@/components/Placeholder.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Placeholder
   }
 };
 </script>
